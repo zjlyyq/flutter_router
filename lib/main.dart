84d2a65e100701a703f7e82aab2fd2ee05ac5388
zjl <jialuzhang0805@gmail.com>
2021-03-19 00:05:47 +0800

    路由和导航： 命名路由的使用 命名路由的传参：只能传字符串，要传多个参数的话需要拼接和解析 命名路由的关闭回调的指定 基本路由也可传参

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,50 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // 注册路由
+      routes: {
+        'second_page':(context) {
+          return Center(
+            child: Text('SecondPage'),
+          );
+        },
+        'third_page': (context) => ThirdPage()
+      },
+      // 错误路由处理，统一返回UnknownPage
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(builder: (context) {
+          return Center(child: Text('404'),);
+        });
+      },
     );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String msg = ModalRoute.of(context).settings.arguments as String;
+    // TODO: implement build
+    return
+      Scaffold(
+          appBar: AppBar(
+            title: Text('大明水果铺', style: TextStyle(color: Color.fromRGBO(146, 32, 46, 1.0)),),
+            backgroundColor: Colors.white,
+          ),
+          body: Column(
+            children: [
+              Text('页面参数：'),
+              Text('${msg}')
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              Navigator.pop(context, '大明水果铺');
+            },
+          ),
+        )
+    ;
   }
 }
 
@@ -29,13 +72,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  String _name = '接受关闭回调参数：';
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,18 +88,41 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            TextButton(onPressed: () {
+              // 页面参数 + 设置关闭回调
+              Navigator.of(context).pushNamed('third_page', arguments: 'Hey!').then((value) {
+                setState(() {
+                  _name += value;
+                });
+              });
+            }, child: Text('传递页面参数')),
+            TextButton(onPressed: () {
+              Navigator.pushNamed(context, 'unknown_page');
+            }, child: Text('go to unknownPage')),
+            TextButton(onPressed: () {
+              Navigator.pushNamed(context, 'second_page');
+            }, child: Text('go to secondPsge')),
             TextButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) {
+                  MaterialPageRoute(
+                    builder: (context) {
+                      String msgs = ModalRoute.of(context).settings.arguments;
+                      String m1 = msgs.split(",")[0];
                       return Center(
-                        child: TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text('go back'),
-                             ),
+                        child: Column(
+                          children: [
+                            Text('基本路由也能获取参数: ${m1}'),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: Text('go back'),
+                            ),
+                          ],
+                        )
                       );
                     },
+                    settings: RouteSettings(name: 'msg', arguments: '基本路由参数1,基本路由参数2')
                   )
                 );
               },
@@ -70,6 +135,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text('$_name')
           ],
         ),
       ),
